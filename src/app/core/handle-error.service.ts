99b2be9696f79752d5d5b86a985cd49027833e72
Thomas Zoratto <thomas.zoratto@gmail.com>
2016-11-11 23:21:55 +0100

    redirect on 401 this will trigger logout if need be

--- src/app/core/handle-error.service.ts
+++ src/app/core/handle-error.service.ts
@@ -3,16 +3,22 @@ import {ResponseService} from './response.service';
 import {MessageService} from './message/message.service';
 import {Observable} from 'rxjs';
 import {Response} from '@angular/http';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class HandleErrorService {
 
     constructor(private responseService: ResponseService,
-                private messageService: MessageService) {
+                private messageService: MessageService,
+                private router: Router) {
 
     }
 
     private handleError(error: Response): Response {
+        if (error.status === 401) {
+            this.router.navigate(['']);
+            return;
+        }
         let message = this.responseService.getErrorMessage(error);
         this.messageService.addAlert(message, 'danger');
         return error;
