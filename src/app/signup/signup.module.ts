b657a38a0f0e9cbb14b2016043ecef452174e875
Thomas Zoratto <thomas.zoratto@gmail.com>
2016-11-12 11:02:32 +0100

    improve design a bit

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -2,10 +2,12 @@ import {NgModule}      from '@angular/core';
 import {SignupComponent} from './signup.component';
 import {SignupGuard} from './signup-guard.service';
 import {TranslateModule} from 'ng2-translate';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     imports: [
-        TranslateModule
+        TranslateModule,
+        FormsModule
     ],
     declarations: [
         SignupComponent
